2a77f029d508a5a0c8e68b2bfb8c34da13f72740
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-13 17:01:07 -0300

    Add news entities files

---
+++ Entities/Entidades/Base.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades
+{
+    public class Base
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+    }
+}
