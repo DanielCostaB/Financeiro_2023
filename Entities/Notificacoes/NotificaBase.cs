d52157994ebfb0127a74c103c704ac34a8219414
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-13 16:38:37 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Notificacoes/NotificaBase.cs
@@ -0,0 +1,18 @@
+﻿namespace Entities.Notificacoes
+{
+    public class NotificaBase
+    {
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+            }
+        }
+    }
+}
