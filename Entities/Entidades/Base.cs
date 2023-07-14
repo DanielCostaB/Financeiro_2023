f4530fb70978b4aabc467eb3246e1bdf672f7031
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-14 16:08:34 -0300

    Add files new Entities and Notifica Tables

--- Entities/Entidades/Base.cs
+++ Entities/Entidades/Base.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Notificacoes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Entidades
 {
-    public class Base
+    public class Base : Notifica
     {
         [Display(Name = "Código")]
         public int Id { get; set; }
