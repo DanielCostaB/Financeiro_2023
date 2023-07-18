6385b5cacc468fa4e88b6f6388a6a877f97b530c
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-18 17:01:03 -0300

    Add files Interfaces

---
+++ Domain/interfaces/Generics/InterfaceGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.interfaces.Generics
+{
+    public interface InterfaceGeneric<T> where T : class
+    {
+        Task Add(T obj);
+        Task Update(T obj);
+        Task Delete(T obj);
+        Task<T> GetEntityById(int Id);
+        Task<List<T>> List();
+    }
+}
