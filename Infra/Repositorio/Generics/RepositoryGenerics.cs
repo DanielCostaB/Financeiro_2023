829adf96e9dc64dd3e7d9aef4f36fb397c0e4de0
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-19 12:41:20 -0300

    Add Files Infra Repositorio Generics

--- Infra/Repositorio/Generics/RepositoryGenerics.cs
+++ Infra/Repositorio/Generics/RepositoryGenerics.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain.interfaces.Generics;
+using Infra.Configuracao;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,42 @@ using System.Threading.Tasks;
 
 namespace Infra.Repositorio.Generics
 {
-    public class RepositoryGenerics
+    public class RepositoryGenerics<T> : InterfaceGeneric<T> where T : class
     {
+        private readonly DbContextOptions<ContextoBase> _OptionsBuilder;
+
+        public RepositoryGenerics() 
+        {
+            _OptionsBuilder = new DbContextOptions<ContextoBase>();
+        }
+
+        public async Task Add(T obj)
+        {
+            using (var data = new ContextoBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(obj);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetEntityById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<T>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
