c3d093fc8c23873cf70ffdebdab5814beca93935
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-19 12:04:37 -0300

    Add files Infra and Repositorio

---
+++ Infra/Configuracao/ContextoBase.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuracao
+{
+    public class ContextoBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextoBase( DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiros { get; set; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiros { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Despesa> Despesa { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetusers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=NBQSP-FC693;Initial Catalog=FINANCEIRO_2023;Integrated Security=False;User ID=sa;Password=1234;Connec Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            
+            //return "Data Source=NBQSP-FC693;Initial Catalog=FINANCEIRO_2023;Integrated Security=True"; // Evitar
+
+        }
+    }
+}
