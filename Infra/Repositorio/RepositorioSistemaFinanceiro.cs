36804833054fef4481153e5ae61c8ced49ea3e2a
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-28 17:07:17 -0300

    Add SistemaFinanceiro Repositorio files news

--- Infra/Repositorio/RepositorioSistemaFinanceiro.cs
+++ Infra/Repositorio/RepositorioSistemaFinanceiro.cs
@@ -1,6 +1,8 @@
 ﻿using Domain.interfaces.ISistemaFinanceiro;
 using Entities.Entidades;
+using Infra.Configuracao;
 using Infra.Repositorio.Generics;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,21 @@ namespace Infra.Repositorio
 {
     public class RepositorioSistemaFinanceiro : RepositoryGenerics<SistemaFinanceiro>, InterfaceSistemaFinanceiro
     {
-        public Task<IList<SistemaFinanceiro>> ListarSistemasUsuario(string emailUsuario)
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+        public RepositorioSistemaFinanceiro()
         {
-            throw new NotImplementedException();
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+        public async Task<IList<SistemaFinanceiro>> ListarSistemasUsuario(string emailUsuario)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await
+                    (from s in banco.SistemaFinanceiro
+                     join us in banco.UsuarioSistemaFinanceiro on s.Id equals us.IdSistema
+                     where us.EmailUsuario.Equals(emailUsuario) 
+                     select s).AsNoTracking().ToListAsync();
+            }
         }
     }
 }
